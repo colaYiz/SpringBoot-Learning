61bf102780282d8e5e4f7e7dcfa13ec72fb2a124
colaYiz <zy9619521@163.com>
2020-07-17 11:37:55 +0800

    Spring-Boot-Starter

---
+++ springbootstartertest/src/main/java/com/cola/SpringbootstartertestApplication.java
@@ -0,0 +1,13 @@
+package com.cola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstartertestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootstartertestApplication.class, args);
+    }
+
+}
---
+++ springbootstartertest/src/test/java/com/cola/SpringbootstartertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstartertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
